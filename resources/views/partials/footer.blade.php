c6abdbbc82cd8066be2446f9a79e58ab79080cc9
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2021-10-27 21:22:23 +0100

    v2.4.5

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="{{ config('app.url') }}" class="text-secondary dim no-underline">{{ config('nova.name') }}</a> {{ __('by') }} <a href="https://essexinteractive.co.uk" class="text-primary dim no-underline">Essex Interactive - Rhys Lees</a> &copy; {{ date('Y') }}<span class="px-1">&middot;</span>
+    <a href="{{ config('app.url') }}" class="text-secondary dim no-underline">{{ config('nova.name') }}</a> {{ __('by') }} <a href="https://manageitpro.co.uk" class="text-primary dim no-underline">Manage It Pro Ltd  - Rhys Lees</a> &copy; {{ date('Y') }}<span class="px-1">&middot;</span>
 </p>    <p class="mt-1 text-center text-xs text-80">
 
-    {{ __('Theme') }} v{{ \EssexInteractive\NovaStyling\Theme::version() }}
+    {{ __('Theme') }} v{{ \ManageItProLtd\NovaStyling\Theme::version() }}
     <span class="px-1">&middot;</span>
     Laravel Nova v{{ \Laravel\Nova\Nova::version() }}
 </p>
