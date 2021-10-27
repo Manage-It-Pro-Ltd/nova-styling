42ba923c31181cca75238c320ad3e57c291a832e
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2021-10-27 21:45:20 +0100

    v2.4.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require("laravel-mix");
 
 mix
-	.js("resources/js/theme.js", "dist/js")
+	// .js("resources/js/theme.js", "dist/js")
 	.sass("resources/sass/dark.scss", "dist/css")
 	.copy("resources/css/manage-it-pro-ltd-theme.css", "dist/css")
 	.copy("resources/css/responsive.css", "dist/css");
